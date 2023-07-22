9a39ca2301bb14846cfcb1fcd7b0c208627fa630
IngridDuraku <ingrid.duraku@fti.edu.al>
2023-07-22 20:24:31 +0200

    feat: m3 implementation

--- models/scripts/m2.py
+++ models/scripts/m2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import zipf
-from ..const import CACHE_SKEW, TOTAL_READS, CPU_H, FIRST_READ_FROM_S3
+from ..const import CACHE_SKEW, TOTAL_READS, CPU_H, FIRST_READ_FROM_S3, SPOOLING_SKEW, SPOOLING_READ_SUM
 
 
 def distr_maker(shape, size):
@@ -78,25 +78,35 @@ def model_distr_pack(bins, distr):
     return res
 
 
-def calc_time_for_config(inst, distr_cache):
+def calc_time_for_config(inst, distr_cache, distr_spooling):
     inst = inst.reset_index()
-    data_mem = pd.DataFrame(data={'size': inst['calc_mem_caching'].round(decimals=0), 'prio': inst['calc_mem_speed']})
-    data_sto = pd.DataFrame(data={'size': inst['calc_sto_caching'].round(decimals=0), 'prio': inst['calc_sto_speed']})
-    data_s3 = pd.DataFrame(data={'size': [len(distr_cache['working'])] * len(inst), 'prio': inst['calc_net_speed']})
-
     bins_cache = {
-        'data_mem': data_mem,
-        'data_sto': data_sto,
-        'data_s3': data_s3
+        'data_mem': pd.DataFrame(data={'size': inst['calc_mem_caching'].round(decimals=0), 'prio': inst['calc_mem_speed']}),
+        'data_sto': pd.DataFrame(data={'size': inst['calc_sto_caching'].round(decimals=0), 'prio': inst['calc_sto_speed']}),
+        'data_s3': pd.DataFrame(data={'size': [len(distr_cache['working'])] * len(inst), 'prio': inst['calc_net_speed']})
+    }
+
+    bins_spooling = {
+        'data_mem': pd.DataFrame(data={'size': inst['calc_mem_spooling'].round(decimals=0), 'prio': inst['calc_mem_speed']}),
+        'data_sto': pd.DataFrame(data={'size': inst['calc_sto_spooling'].round(decimals=0), 'prio': inst['calc_sto_speed']}),
+        'data_s3': pd.DataFrame(data={'size': [len(distr_spooling)] * len(inst), 'prio': inst['calc_net_speed']})
     }
 
     mem_read_distribution = model_distr_pack(bins_cache, distr_cache['working'])
+    spool_read_distribution = model_distr_pack(bins_spooling, distr_spooling)
+
     cpu_time = CPU_H / inst['vcpu_count']
     inst['data_mem'] = mem_read_distribution['data_mem']
     inst['data_sto'] = mem_read_distribution['data_sto']
     inst['data_s3'] = mem_read_distribution['data_s3']
 
-    scan_time = mem_read_distribution['data_mem'] / inst['calc_mem_speed'] + mem_read_distribution['data_sto'] / inst['calc_sto_speed'] + mem_read_distribution['data_s3'] / inst['calc_net_speed']
+    inst['spooling_mem'] = spool_read_distribution['data_mem']
+    inst['spooling_sto'] = spool_read_distribution['data_sto']
+    inst['spooling_s3'] = spool_read_distribution['data_s3']
+
+    scan_time = (mem_read_distribution['data_mem'] + 2 * spool_read_distribution['data_mem'] / inst['calc_mem_speed']) \
+                + (mem_read_distribution['data_sto'] + 2 * spool_read_distribution['data_sto'] / inst['calc_sto_speed']) \
+                + mem_read_distribution['data_s3'] + 2 * spool_read_distribution['data_s3'] / inst['calc_net_speed']
     inst['execution_time_m2'] = cpu_time + scan_time
 
     return inst
@@ -105,7 +115,8 @@ def calc_time_for_config(inst, distr_cache):
 def calc_time(instances):
     distr_caching_precomputed = distr_maker(shape=CACHE_SKEW, size=TOTAL_READS)
     distr_cache = model_distr_split_fn(distr_caching_precomputed, FIRST_READ_FROM_S3)
-    instances = calc_time_for_config(instances, distr_cache)
+    spooling_distr = distr_maker(shape=SPOOLING_SKEW, size=SPOOLING_READ_SUM)
+    instances = calc_time_for_config(instances, distr_cache, spooling_distr)
 
     return instances
 
